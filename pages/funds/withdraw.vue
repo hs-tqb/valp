7d440575b540eab005d4aeabf4dac4678997911a
HS <huangshuai@utianqi.com>
2018-04-13 15:08:49 +0800

    提现的提示文案

--- pages/funds/withdraw.vue
+++ pages/funds/withdraw.vue
@@ -74,7 +74,7 @@ export default {
           value:'',
           type:'number',
           required:true,
-          explain:'(不得少于0.1个)',
+          explain:'(至少0.1个)',
           warning: 'ETH 数量不正确',
           verify() {
             return !isNaN( +this.value.trim() );
