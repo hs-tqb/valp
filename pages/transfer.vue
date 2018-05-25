39afa6fba5a924610461badd3b2c4239fa109dd1
HS <huangshuai@utianqi.com>
2018-05-25 13:50:01 +0800

    转账说明

--- pages/transfer.vue
+++ pages/transfer.vue
@@ -1,9 +1,20 @@
 <style lang="less">
+
   #__nuxt, #__layout, #page-root { min-width:auto;  height:100%; }
   #page-transfer {
-    min-height:100%;
     background:url(~/assets/img/banner.jpg) no-repeat center / cover;
-    display:flex; flex-direction:column; justify-content:center; align-items:center;
+    height:100%;
+    overflow:auto;
+    -webkit-overflow-scrolling:touch;
+    .d1 { 
+      height:90vh; 
+      display:flex; flex-direction:column; justify-content:center; align-items:center;
+    }
+    .d2 {
+      // display:flex; flex-direction:column; justify-items:center; justify-content:center;
+      padding:10px 0;
+      img { display:block; margin:10px auto; }
+    }
     h1 { margin:-2em 0 2em 0; font-size:30px; color:#fff; }
     .center { 
       width:36em; color:#fff; 
@@ -19,16 +30,23 @@
     .center {
       width:70%;
     }
+    .d2 { padding:10px; }
   }
 </style>
 
 
 <template>
   <div id="page-transfer">
-    <h1>兑换 VPP</h1>
-    <div class="center">
-      <img src="~/assets/img/transfer/qrcode.png" alt="">
-      <p>用以太坊钱包扫码转账或将ETH转向地址：<a class="address" target="_blank" href="https://etherscan.io/address/0x6f89544bc460f15315086f411b9c1fe5c8cfb1f1">0x6f89544bc460f15315086f411b9c1fe5c8cfb1f1</a>，智能合约将自动按1:52000兑换率将VPP转到您钱包，请稍后登录钱包查看VPP余额。如对交易有疑问请联系: <a href="tel:0755-33530815">0755-33530815</a></p>
+    <div class="d1">
+      <h1>兑换 VPP</h1>
+      <div class="center">
+        <img src="~/assets/img/transfer/qrcode.png" alt="">
+        <p>用以太坊钱包扫码转账或将ETH转向地址：<a class="address" target="_blank" href="https://etherscan.io/address/0x6f89544bc460f15315086f411b9c1fe5c8cfb1f1">0x6f89544bc460f15315086f411b9c1fe5c8cfb1f1</a>，智能合约将自动按1:52000兑换率将VPP转到您钱包，请稍后登录钱包查看VPP余额。转账前务必看下图中的说明，否则交易可能失败。如对交易有疑问请联系: <a href="tel:0755-33530815">0755-33530815</a></p>
+      </div>
+    </div>
+    <div class="d2">
+      <img src="~/assets/img/transfer/e1.jpg" alt="">
+      <img src="~/assets/img/transfer/e2.jpg" alt="">
     </div>
   </div>
 </template>
